6830c10ce93316fc5e344874f391d5e801da711e
Kathy Hudson <khitx@icloud.com>
2018-01-13 22:42:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(names)
   names.each_with_index {|name,i| puts "#{i+1} #{name}"}
 end
 
-def summon_captain_planet
-
+def summon_captain_planet(calls)
+  calls.collect {|call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls
