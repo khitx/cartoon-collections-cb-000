abceb35c6d656c7f4ec5064a799b8da53ae1b42e
Kathy Hudson <khitx@icloud.com>
2018-01-13 22:36:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,13 @@
-def roll_call_dwarves
-  
+def roll_call_dwarves(names)
+  names.each_with_index {|name,i| puts "#{i+1} #{name}"}
 end
 
 def summon_captain_planet
-  
+
 end
 
 def long_planeteer_calls
-  
+
 end
 
 def find_the_cheese
